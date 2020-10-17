a0c4d51653e0d142094a7526cad2c7a2f9365b70
Efrain May <wwifihack@gmail.com>
2020-10-16 22:09:58 -0500

    Docs Agregado

---
+++ docs/precache-manifest.26e1acc493fd55d37c3fa7176ee482d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbf53d583975b0e83855df4a350d0b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be576888607faaef9b82",
+    "url": "/static/css/main.e0f45a58.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be576888607faaef9b82",
+    "url": "/static/js/main.10f017ba.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
